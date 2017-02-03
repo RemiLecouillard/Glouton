b399d9de5e82ab278c00e72478e562efc97109e5
Maxime <m.leplanquois@hotmail.fr>
2017-02-04 00:53:29 +0100

    Page d'accueil toute stylée
    Glouton

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LE GLOUTON</title>
+		<meta name="publisher" content="Dussartre Lecouillard Leplanquois">
+		<meta name="description" content="Le meilleur site des internets, dédié au meilleur animal ayant jamais gracié cette terre!">
+		<link rel="stylesheet" href="gloustyle.css" type="text/css"/>
+	</head>
+	<body>
+		<div id="gloutHeader">
+			<img id="glouGauche" src="images/glouTete.png"/>
+			<img id="glouDroit" src="images/glouTete.png"/>
+		</div>
+		<h1 id="glouTitre">LE GLOUTON <span id="exclaGlouton">!</span></h1>
+		<img id="gloutImage" src="images/glout1.jpg"/>
+	</body>
+</html>
