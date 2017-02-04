9364873d43ce68fe331e2c2208f3086ab52c22b9
Mavel <maveldu@orange.fr>
2017-02-04 16:35:26 +0100

    Footer+favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<meta name="publisher" content="Dussartre Lecouillard Leplanquois">
 		<meta name="description" content="Le meilleur site des internets, dédié au meilleur animal ayant jamais gracié cette terre!">
 		<link rel="stylesheet" href="gloustyle.css" type="text/css"/>
+		<link rel="icon" type="image/jpg" href="favicon.png"/>
 	</head>
 	<body>
 		<div id="gloutHeader">
@@ -13,5 +14,13 @@
 		</div>
 		<h1 id="glouTitre">LE GLOUTON <span id="exclaGlouton">!</span></h1>
 		<img id="gloutImage" src="images/glout1.jpg"/>
+		<div id="gloutMenu">
+			<a href="about.php"><button>About</button></a>
+			<a href="gloutmap.php"><button>Where to find them?</button></a>
+			<a href="contact.php"><button>Contact</button></a>
+		</div>
 	</body>
+	<footer>
+		Copyright © 2017 Glouton corporation
+	</footer>
 </html>
